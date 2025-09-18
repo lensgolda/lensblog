7e8ad40ed9ec328ffac3e7482f06a6017956fddd
Aleksey Golda <aleksey.golda@flocktory.com>
2025-09-18 18:17:52 +0200

    fix

--- _config.ts
+++ _config.ts
@@ -1,7 +1,9 @@
 import lume from "lume/mod.ts";
 import blog from "blog/mod.ts";
 
-const site = lume();
+const site = lume({
+    basePath: Deno.env.get("GH_PAGES") ? "/lensblog" : "",
+});
 
 site.use(blog());
 
